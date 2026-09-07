f32f87ac6da7aea87c3e78465f22a6307a8d0bd2
brenobezerra2014-cyber <brenobezerra2014@gmail.com>
2026-09-07 16:07:08 -0300

    classe usuario

--- Usuario.java
+++ Usuario.java
@@ -37,5 +37,62 @@ public class Usuario {
     }
     public void setCpf(String cpf) {
         this.cpf = cpf;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;  
+    }
+    public char getSexo() {
+        return sexo;
+    }
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;  
+    }
+    public String getNomeCartao() {
+        return nomeCartao;
+    }
+    public void setNomeCartao(String nomeCartao) {
+        this.nomeCartao = nomeCartao;
+    }
+    public String getNumeroCartao() {
+        return numeroCartao;
+    }
+    public void setNumeroCartao(String numeroCartao) {
+        this.numeroCartao = numeroCartao;
+    }
+    public String getCvv() {
+        return cvv;
+    }
+    public void setCvv(String cvv) {
+        this.cvv = cvv;
+    }
+    public List<Compra> getCompras() {
+        return compras;
+    }
+    public void setCompras(List<Compra> compras) {
+        this.compras = compras;
+    }
+    
+    public void adicionarCompra(Compra compra) {
+        compras.add(compra);
+    }
+    public void removerCompra(Compra compra) {
+        compras.remove(compra);
+    }
     
 }
+    
