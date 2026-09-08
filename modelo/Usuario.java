ff8d6450eea4f61607c5c8c029be5260c8960d8f
allages <allslages@gmail.com>
2026-09-08 08:32:05 -0300

    Compra criada e Usuário incompleto

--- modelo/Usuario.java
+++ modelo/Usuario.java
@@ -1,3 +1,8 @@
+package modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Usuario {
     private String user;
     private String cpf;
@@ -8,9 +13,8 @@ public class Usuario {
     private String nomeCartao;
     private String numeroCartao;
     private String cvv;
-    private List <Compra> compras;
-    
-    
+    private List<Compra> compras;
+
     public Usuario(String user, String cpf, String senha, char sexo, String email, int idade, String nomeCartao, String numeroCartao, String cvv) {
         this.user = user;
         this.cpf = cpf;
@@ -22,77 +26,104 @@ public class Usuario {
         this.numeroCartao = numeroCartao;
         this.cvv = cvv;
         this.compras = new ArrayList<>();
-        
-    
     }
-    
+
+
+
+    //Gets e Sets
+
+
     public String getUser() {
         return user;
     }
+
     public void setUser(String user) {
         this.user = user;
     }
+
     public String getCpf() {
         return cpf;
     }
+
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
+
     public String getSenha() {
         return senha;
     }
+
     public void setSenha(String senha) {
-        this.senha = senha;  
+        this.senha = senha;
     }
+
     public char getSexo() {
         return sexo;
     }
+
     public void setSexo(char sexo) {
         this.sexo = sexo;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public int getIdade() {
         return idade;
     }
+
     public void setIdade(int idade) {
-        this.idade = idade;  
+        this.idade = idade;
     }
+
     public String getNomeCartao() {
         return nomeCartao;
     }
+
     public void setNomeCartao(String nomeCartao) {
         this.nomeCartao = nomeCartao;
     }
+
     public String getNumeroCartao() {
         return numeroCartao;
     }
+
     public void setNumeroCartao(String numeroCartao) {
         this.numeroCartao = numeroCartao;
     }
+
     public String getCvv() {
         return cvv;
     }
+
     public void setCvv(String cvv) {
         this.cvv = cvv;
     }
+
     public List<Compra> getCompras() {
         return compras;
     }
+
     public void setCompras(List<Compra> compras) {
         this.compras = compras;
     }
-    
+
+
+
+    // Funções da Classe
+
+
+
     public void adicionarCompra(Compra compra) {
         compras.add(compra);
     }
+
     public void removerCompra(Compra compra) {
         compras.remove(compra);
     }
-    
 }
-    
