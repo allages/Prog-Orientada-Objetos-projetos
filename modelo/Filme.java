4658bc8430b2275007f4f8042b06ac6e358eab89
allages <allslages@gmail.com>
2026-09-08 10:11:29 -0300

    Classe Filme completo

---
+++ modelo/Filme.java
@@ -0,0 +1,37 @@
+package modelo;
+
+public class Filme {
+    private String nome;
+    private int duracao;
+    private  String sinopse;
+    private double valor;
+
+    public Filme(String nome, int duracao, String sinopse, double valor) {
+        this.nome = nome;
+        this.duracao = duracao;
+        this.sinopse = sinopse;
+        this.valor = valor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+}
+
